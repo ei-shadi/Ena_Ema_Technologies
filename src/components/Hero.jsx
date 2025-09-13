624eaefd25d7fed4141b7d7cfb30f989bcc4aa23
ei-shadi <eftajulislam.saadi@gmail.com>
2025-09-13 16:19:56 +0600

    Fix some Error

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,108 +10,110 @@ import { GoStar } from "react-icons/go";
 
 const Hero = () => {
   return (
-    <section className="min-h-screen w-full relative bg-[#151f1b]">
+    <section className="min-h-screen w-full relative bg-[#151f1b] px-5 md:mx-0">
       <div
         className="absolute inset-0 z-0"
         style={{
-  background: "radial-gradient(ellipse 60% 70% at 50% 0%, rgba(31,255,165,0.13), transparent 70%), #151f1b",
-}}
+          background: "radial-gradient(ellipse 60% 70% at 50% 0%, rgba(31,255,165,0.13), transparent 70%), #151f1b",
+        }}
 
       />
       {/* Logo */}
       <Image
         src={Logo}
         alt="Logo"
-        className="w-[120px] h-[27px] absolute top-[5%] left-1/2 -translate-x-1/2 -translate-y-1/2 z-10"
+        className="w-[80px] h-[18px] md:w-[120px] md:h-[27px] absolute top-[4%] md:top-[5%] left-1/2 -translate-x-1/2 -translate-y-1/2 z-10"
       />
 
       {/* Other Content */}
-      <div className="absolute z-10 top-[15%] left-1/2 -translate-x-1/2 ">
+      <div className="absolute z-10 top-[10%] md:top-[15%] left-1/2 -translate-x-1/2 ">
         {/* Headline and Description */}
         <div className="mb-12">
           {/* Badge */}
           <div className="flex items-center gap-2 bg-[#192420] px-4 py-2 rounded-full border-t-[1px] border-[#1FFFA5] w-[334px] mx-auto mb-6">
-            <PiSparkleBold className="w-[15px] h-[15px] text-[#1FFFA5]"/>
+            <PiSparkleBold className="w-[15px] h-[15px] text-[#1FFFA5]" />
             <p className="text-[#B2B2B2] text-xs">Trusted by 30+ startups & entrepreneurs worldwide</p>
           </div>
 
           {/* Headline */}
           <div>
-            <h1 className="text-[#F5F5F5] w-[1050px] text-7xl text-center font-bold pb-4 font-manrope">Strategic Landing Pages That Convert From Just <span className="text-[#1FFFA5]">$200</span>
-          </h1>
+            <h1 className="text-[#F5F5F5] mx-auto w-[361px] md:w-[614px] lg:w-[1050px] text-[36px] md:text-[64px] lg:text-7xl text-center font-bold pb-4 font-manrope leading-tight">Strategic Landing Pages That Convert From Just <span className="text-[#1FFFA5]">$200</span>
+            </h1>
 
-          <p className="text-[#F5F5F5] text-center max-w-[840px] mx-auto">Stop wasting traffic on generic pages. We deliver premium, conversion-focused landing pages designed, developed, and launched in just 5 days.</p>
+            <p className="text-[#F5F5F5] text-xs md:text-lg text-center md:w-[614px] lg:max-w-[840px] mx-auto">Stop wasting traffic on generic pages. We deliver premium, conversion-focused landing pages designed, developed, and launched in just 5 days.</p>
           </div>
         </div>
 
         {/* Input Field And Button */}
-        <div>
+        <div className="">
 
-{/* Container for input field and button */}
-<div className="border-[1px] border-[#2A3C36] p-9 max-w-[794px] mx-auto rounded-lg shadow-2xl shadow-[rgba(31,255,165,0.1)]">
-{/* Input fields */}
-<div className=" flex justify-center items-center gap-4 mb-6">
-{/* User Input Field */}
-<div className="flex items-center relative">
-<Image src={User} alt="User" className="w-[14px] h-[14px] absolute top-1/2 left-5 -translate-y-1/2"/>
-<input type="text" 
-className="py-[18px] pl-10 pr-14 text-xs  text-[#B2B2B2] border-[1px] border-[#2A3C36] rounded-lg focus:outline-none focus:border-[#1FFFA5]"
-placeholder="Your name"/>
-</div>
+          {/* Container for input field and button */}
+          <div className="border-[1px] border-[#2A3C36] p-4 md:p-9 w-[340px] md:w-[614px] lg:w-[794px] mx-auto rounded-lg shadow-2xl shadow-[rgba(31,255,165,0.1)] ">
+            {/* Input fields */}
+            <div className=" flex flex-col md:flex-row justify-center items-center gap-4 mb-6">
+              {/* User Input Field */}
+              <div className="flex items-center relative">
+                <Image src={User} alt="User" className="w-[14px] h-[14px] absolute top-1/2 left-5 -translate-y-1/2" />
+                <input type="text"
+                  className="py-[18px] pl-10 pr-32 md:pr-0 lg:pr-14 text-xs  text-[#B2B2B2] border-[1px] border-[#2A3C36] rounded-[4px] focus:outline-none focus:border-[#1FFFA5]"
+                  placeholder="Your name" />
+              </div>
 
-{/* Email Input Field */}
-<div className="flex items-center relative">
-<Image src={Email} alt="User" className="w-[14px] h-[14px] absolute top-1/2 left-5 -translate-y-1/2"/>
-<input type="text" 
-className="py-[18px] pl-10 pr-14 text-xs text-[#B2B2B2] border-[1px] border-[#2A3C36] rounded-lg focus:outline-none focus:border-[#1FFFA5]"
-placeholder="Email Address"/>
-</div>
+              {/* Email Input Field */}
+              <div className="flex items-center relative">
+                <Image src={Email} alt="User" className="w-[14px] h-[14px] absolute top-1/2 left-5 -translate-y-1/2" />
+                <input type="text"
+                  className="py-[18px] pl-10 pr-32 md:pr-0 lg:pr-14 text-xs text-[#B2B2B2] border-[1px] border-[#2A3C36] rounded-[4px] focus:outline-none focus:border-[#1FFFA5]"
+                  placeholder="Email Address" />
+              </div>
 
-{/* Number Input Field */}
-<div className="flex items-center relative">
-<Image src={Phone} alt="User" className="w-[14px] h-[14px] absolute top-1/2 left-5 -translate-y-1/2"/>
-<input type="text" 
-className="py-[18px] pl-10 pr-14 text-xs text-[#B2B2B2] border-[1px] border-[#2A3C36] rounded-lg focus:outline-none focus:border-[#1FFFA5]"
-placeholder="Phone Number"/>
-</div>
-
-
-</div>
-
-{/* Button */}
-<div>
-<Button>
-  Get my high-converting landing page
-</Button>
-</div>
-
-</div>
-
-{/* Text & Icons */}
-<div className="flex items-center gap-16 justify-center mt-4">
-{/* Lock Icon */}
-<div className="flex items-center gap-2 text-xs text-[#B2B2B2]">
-<PiLock />
-<p>SSL secure</p>
-</div>
+              {/* Number Input Field */}
+              <div className="flex items-center relative">
+                <Image src={Phone} alt="User" className="w-[14px] h-[14px] absolute top-1/2 left-5 -translate-y-1/2" />
+                <input type="text"
+                  className="py-[18px] pl-10 pr-32 md:pr-0 lg:pr-14 text-xs text-[#B2B2B2] border-[1px] border-[#2A3C36] rounded-[4px] focus:outline-none focus:border-[#1FFFA5]"
+                  placeholder="Phone Number" />
+              </div>
 
 
-{/* Money Back Icon */}
-<div className="flex items-center gap-2 text-xs text-[#B2B2B2]">
-<TfiCheck />
-<p>Money-back guaranteed</p>
-</div>
+            </div>
+
+            {/* Button */}
+            <div className="w-[300px] mx-auto md:w-full">
+              <Button>
+                Get my high-converting landing page
+              </Button>
+            </div>
+
+          </div>
+
+          {/* Text & Icons */}
+          <div className="flex flex-col md:flex-row md:items-center justify-center px-4 mt-4 max-w-[630px] mx-auto gap-0 md:gap-16">
+            <div className="flex items-center gap-24 md:gap-16">
+              {/* Lock Icon */}
+              <div className="flex items-center gap-2 text-xs text-[#B2B2B2]">
+                <PiLock />
+                <p>SSL secure</p>
+              </div>
 
 
-{/* Satisfaction Icon */}
-<div className="flex items-center gap-2 text-xs text-[#B2B2B2]">
-<GoStar />
-<p>Satisfaction guaranteed</p>
-</div>
+              {/* Money Back Icon */}
+              <div className="flex items-center gap-2 text-xs text-[#B2B2B2]">
+                <TfiCheck />
+                <p>Money-back guaranteed</p>
+              </div>
+            </div>
 
 
-</div>
-      </div>
+            {/* Satisfaction Icon */}
+            <div className="flex items-center gap-2 mt-2 md:mt-0 text-xs text-[#B2B2B2]">
+              <GoStar />
+              <p>Satisfaction guaranteed</p>
+            </div>
+
+
+          </div>
+        </div>
       </div>
 
     </section>
--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Button = ({ children }) => {
   return (
     <button className="w-full flex items-center justify-center gap-3 px-6 py-4 bg-[#1FFFA5] rounded-lg shadow-[0_9px_24px_0_rgba(31,255,165,0.25),inset_0_7px_8px_0_rgba(255,255,255,0.5)] hover:bg-[#002013] hover:text-white text-[#002013] cursor-pointer">
-      <p className="font-semibold text-lg">{children}</p>
+      <p className="font-semibold text-xs md:text-lg">{children}</p>
 
       <Image
         src={Arrow}
